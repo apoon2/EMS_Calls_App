41c744fc9ce7b4de6033c8e27c0809e5cf40caa4
Ashley Poon <poon.ashley95@gmail.com>
2021-05-21 18:20:14 -0400

    updates to app

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -7,12 +7,8 @@ st.title('NYC Emergency Call Volume Estimator')
 # About Section
 st.write('''Can we predict emergency call volume in NYC using time, weather, and traffic data? While these features are beyond our control, the goal of this project is to increase preparedness for a surge in emergency services given specific conditions. Using predictive modeling, we hope to be able to provide an indication of when the city should increase staff in emergency call centers or even on response teams.
 ___
-### Datasets:
-[EMS](https://data.cityofnewyork.us/Public-Safety/EMS-Incident-Dispatch-Data/76xm-jjuj) | [Weather](https://www.ncdc.noaa.gov/cdo-web/search) | [Traffic](https://data.ny.gov/Transportation/511-NY-Events-Beginning-2010/ah74-pg4w)
-
-We first combined data from these three different sources to get our training dataset.
-
 ### Data Dictionary:
+
 | Feature                   | Notes                                         |
 | ---                       | ---                                           |
 | Month                     | Month of the year (1-12)                      |
@@ -21,15 +17,11 @@ We first combined data from these three different sources to get our training da
 | Precipitation             | Precipitation by intensity (1-3 : L,M,H)      |
 | Snow Depth                | Snow depth on ground in inches                |
 | Temp (F)                  | Temperature in Fahrenheit                     |
-| Traffic Incidents         | Number of traffic incidents within the hour   |
+| Incidences                | Number of traffic incidents within the hour   |
+___
+### Datasets:
 
-These were the final features used in modeling.
-
-### Results:
-The best performing model used here was a random forest with a max_depth of 5 and n_estimators of 250. The model had train and test r2 scores of 0.81 and a RMSE of 24 calls. Overall, this model had a relatively high score with no overfitting.
-
-### For More Details:
-[Github](https://github.com/dsi-group1/nyc-emergency-call-volume)
+[EMS](https://data.cityofnewyork.us/Public-Safety/EMS-Incident-Dispatch-Data/76xm-jjuj) | [Weather](https://www.ncdc.noaa.gov/cdo-web/search) | [Traffic](https://data.ny.gov/Transportation/511-NY-Events-Beginning-2010/ah74-pg4w)
 
 ### Contributors:
 [Chris Caress](https://www.linkedin.com/in/chris-caress-4245a51b5/) | [Christina Holland](https://www.linkedin.com/in/christina-holland-7400a1140/) | [Ashley Poon](https://www.linkedin.com/in/ashley-poon-y95/) | [David Romo](https://www.linkedin.com/in/daromo/)
@@ -67,7 +59,7 @@ Incidences = st.sidebar.slider('Traffic Incidents',0,53) #get actual range for t
 params = [[year, month, day, hour, PRCP, SNOW, SNWD, TAVG_CALC, Incidences]]
 
 # import  model
-with open('app-model.pkl', mode='rb') as pickle_in:
+with open('../modeling/app-model.pkl', mode='rb') as pickle_in:
     model = pickle.load(pickle_in)
 
 # make prediction
